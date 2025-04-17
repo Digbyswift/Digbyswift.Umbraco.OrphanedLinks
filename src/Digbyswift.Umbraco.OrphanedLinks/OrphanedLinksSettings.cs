4de790207ccf78daebb41f86c33fdab830071b07
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2025-04-17 17:19:44 +0100

    Add OrphanedLinksSettings class

---
+++ src/Digbyswift.Umbraco.OrphanedLinks/OrphanedLinksSettings.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable SA1402
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace Digbyswift.Umbraco.OrphanedLinks;
+
+public class OrphanedLinksSettings
+{
+    public const string SectionName = "Digbyswift:OrphanedLinks";
+
+    [ConfigurationKeyName("Enabled")]
+    public bool IsEnabled { get; set; } = true;
+
+    public OrphanedContentLinksSettings Content { get; set; } = new();
+    public OrphanedMediaLinksSettings Media { get; set; } = new();
+}
+
+public class OrphanedContentLinksSettings
+{
+    [ConfigurationKeyName("Enabled")]
+    public bool IsEnabled { get; set; } = true;
+
+    public IEnumerable<string> IncludedDocTypes { get; set; } = [];
+    public IEnumerable<string> ExcludedDocTypes { get; set; } = [];
+}
+
+public class OrphanedMediaLinksSettings
+{
+    [ConfigurationKeyName("Enabled")]
+    public bool IsEnabled { get; set; } = true;
+
+    public IEnumerable<string> IncludedMediaTypes { get; set; } = [];
+    public IEnumerable<string> ExcludedMediaTypes { get; set; } = [];
+}
